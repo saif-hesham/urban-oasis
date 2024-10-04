8997d0e251057ebfd54f37ab3cf2debc33a556db
Saif Hesham <saifeldeen.hesham@gmial.com>
2024-10-04 17:51:31 +0300

    Refactor handlers to remove all try and catch blocks and use a more centralized approach

--- urban-oasis-server/src/handlers/apartment-handlers.ts
+++ urban-oasis-server/src/handlers/apartment-handlers.ts
@@ -1,74 +1,61 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express';
 import Apartment, {
   ApartmentType,
   ApartmentWithId,
-} from "../models/apartment.model";
-import { GetApartmentsRequest, GetApartmentsResponse } from "../types/types";
-import { ParamsWithId } from "./../schemas/apartment-zod-schemas";
+} from '../models/apartment.model';
+import { GetApartmentsRequest, GetApartmentsResponse } from '../types/types';
+import { ParamsWithId } from './../schemas/apartment-zod-schemas';
+import asyncHandler from './../utils/utils';
 
-export const getApartments = async (
+export const getApartments = asyncHandler(async (
   req: Request<{}, {}, {}, GetApartmentsRequest>,
   res: Response<GetApartmentsResponse>,
   next: NextFunction
 ) => {
-  try {
-    const { page = 1, limit = 10, unitName, unitNumber, project } = req.query;
-    let query = Apartment.find();
+  const { page = 1, limit = 10, unitName, unitNumber, project } = req.query;
+  let query = Apartment.find();
 
-    if (unitName) {
-      query = query.where("unitName").regex(new RegExp(unitName, "i"));
-    } else if (unitNumber) {
-      query = query.where("unitNumber").equals(unitNumber);
-    } else if (project) {
-      query = query.where("project").regex(new RegExp(project, "i"));
-    }
-
-    const totalDocuments = await Apartment.countDocuments(query.getFilter());
-    const totalPages = Math.ceil(totalDocuments / limit);
-    
-    const skipCount = (page - 1) * limit;
-    query = query.skip(skipCount).limit(limit);
-    const apartments = await query;
-
-    res
-      .status(200)
-      .json({
-        count: apartments.length,
-        currentPage: page,
-        totalPages,
-        data: apartments,
-      });
-  } catch (error) {
-    next(error);
+  if (unitName) {
+    query = query.where("unitName").regex(new RegExp(unitName, "i"));
+  } else if (unitNumber) {
+    query = query.where("unitNumber").equals(unitNumber);
+  } else if (project) {
+    query = query.where("project").regex(new RegExp(project, "i"));
   }
-};
 
-export const findApartmentById = async (
+  const totalDocuments = await Apartment.countDocuments(query.getFilter());
+  const totalPages = Math.ceil(totalDocuments / limit);
+  
+  const skipCount = (page - 1) * limit;
+  query = query.skip(skipCount).limit(limit);
+  const apartments = await query;
+
+  res.status(200).json({
+    count: apartments.length,
+    currentPage: page,
+    totalPages,
+    data: apartments,
+  });
+});
+
+export const findApartmentById = asyncHandler(async (
   req: Request<ParamsWithId>,
   res: Response<ApartmentWithId>,
   next: NextFunction
 ) => {
-  try {
-    const apartment = await Apartment.findById(req.params.id);
-    if (!apartment) {
-      res.status(404);
-      throw new Error(`Apartment with id ${req.params.id} was not found`);
-    }
-    res.status(200).json(apartment);
-  } catch (error) {
-    next(error);
+  const apartment = await Apartment.findById(req.params.id);
+  if (!apartment) {
+    res.status(404);
+    throw new Error(`Apartment with id ${req.params.id} was not found`);
   }
-};
+  res.status(200).json(apartment);
+});
 
-export const createApartment = async (
+export const createApartment = asyncHandler(async (
   req: Request<{}, ApartmentWithId, ApartmentType>,
   res: Response<ApartmentWithId>,
   next: NextFunction
 ) => {
-  try {
-    const apartment = await Apartment.create(req.body);
-    res.status(201).json(apartment);
-  } catch (error) {
-    next(error);
-  }
-};
+  const apartment = await Apartment.create(req.body);
+  res.status(201).json(apartment);
+});
---
+++ urban-oasis-server/src/utils/utils.ts
@@ -0,0 +1,10 @@
+// src/utils/asyncHandler.ts
+import { NextFunction, Request, Response } from 'express';
+
+const asyncHandler = (fn: Function) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
+
+export default asyncHandler;
